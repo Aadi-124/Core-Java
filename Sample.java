24f355992972060f4316e24ae51c0824261dc632
Aadi-124 <aadityathakare124@gmail.com>
2023-06-24 18:01:18 +0530

    Initial Commit!

--- Sample.java
+++ Sample.java
@@ -1,8 +1,15 @@
+
 import java.util.*;
-class Sample
+
+public class Sample
 {
-    public static void main(String[] args) {
+	public static void main(String[] args)
+    {
         System.out.println("Hello World");
-		// This is the Change that i have made!
     }
 }
+
+
+
+
+
