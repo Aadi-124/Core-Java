8f4f76a01332775b3f88d2f56482077048d07a66
Aadi-124 <aadityathakare124@gmail.com>
2023-06-26 21:57:48 +0530

    For each loop added!

---
+++ ForeachLoop.java
@@ -0,0 +1,10 @@
+public class ForeachLoop {
+    public static void main(String[] args)
+    {
+        String[] arr = {"Aaditya","Shlok","Jack","James"};
+        for (String x : arr)
+        {
+            System.out.println(x);
+        }
+    }
+}
